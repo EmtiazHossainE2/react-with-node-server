b2a0f1f211b7ea7f0af9d66b18c7e10956f6f55a
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-21 13:07:00 +0600

    basic concept practice

--- __steps.js
+++ __steps.js
@@ -1,22 +1,15 @@
-# React-with-node-server 
-
-
+/**
  * folder banay npm init -y ; npm install express , script e [package.json] 
     "start" : "node index.js",
     "start-dev" : "nodemon index.js",
 
  * index.js => 
-
     const express = require('express');
     const app = express()
     const port = process.env.PORT || 5000
 
     app.get('/', (req, res) => {
         res.send('Module 64 practice ')
-    })
-
-    app.listen(port, () => {
-        console.log(`listening on port `, port)
     })
  * 
  * 
